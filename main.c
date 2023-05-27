a5bba68f8d1dff0f6b0478525c04d0497e8d0173
nikyoff <nikyoff@gmail.com>
2023-05-27 13:05:02 +0600

    chore: initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+
+    printf("Write a: ");
+    scanf("%i", &a);
+
+    printf("Write b: ");
+    scanf("%i", &b);
+
+    printf("\nSum: %d\n", a + b);
+
+    return 0;
+}
